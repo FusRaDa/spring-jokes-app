09e988c698d274aeff9b03dbd5994176011d7778
Matthew Rada <matthewr7.christopher@gmail.com>
2023-02-07 18:32:47 -0500

    first commit

---
+++ src/test/java/chucknorrisjokes/chucknorris/ChucknorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package chucknorrisjokes.chucknorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
